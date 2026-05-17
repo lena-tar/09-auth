6bda7a3bfe543b85dfc60b2e63332fc3513a4328
lena-tar <taran9933@gmail.com>
2026-05-17 15:01:52 +0200

    change proxy

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { parse } from "cookie";
 import { checkServerSession } from "./lib/api/serverApi";
 
 const privateRoutes = ["/profile", "/notes"];
@@ -27,16 +26,11 @@ export async function proxy(request: NextRequest) {
       if (setCookie) {
         const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
         for (const cookieStr of cookieArray) {
-          const parsed = parse(cookieStr);
-          const options = {
-            expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
-            path: parsed.Path,
-            maxAge: Number(parsed["Max-Age"]),
-          };
-          if (parsed.accessToken)
-            cookieStore.set("accessToken", parsed.accessToken, options);
-          if (parsed.refreshToken)
-            cookieStore.set("refreshToken", parsed.refreshToken, options);
+          const [nameValue] = cookieStr.split(";");
+          const [name, value] = nameValue.trim().split("=");
+          if (name && value) {
+            cookieStore.set(name.trim(), value.trim());
+          }
         }
         if (isPublicRoute) {
           return NextResponse.redirect(new URL("/", request.url), {
